bcd75c0f4c977e72d467a395afa320b68f982798
Negin <nuggetnegin@gmail.com>
2019-11-28 12:45:04 -0500

    Updates

---
+++ precache-manifest.22fba7649074346aae3d45899bfcf7af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b15c82085b6b6a3d4a50e841944e2b",
+    "url": "/wishyWashy/index.html"
+  },
+  {
+    "revision": "0e0351d7f380f83175ce",
+    "url": "/wishyWashy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "da7e141cb760c210b5fd",
+    "url": "/wishyWashy/static/js/2.1bd5f919.chunk.js"
+  },
+  {
+    "revision": "0e0351d7f380f83175ce",
+    "url": "/wishyWashy/static/js/main.b245dab7.chunk.js"
+  },
+  {
+    "revision": "aa8ba3b43bdaa7bf727d",
+    "url": "/wishyWashy/static/js/runtime-main.b5eb226b.js"
+  }
+]);
