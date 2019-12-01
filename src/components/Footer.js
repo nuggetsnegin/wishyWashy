0e46c6f58bf4c03b19d41ed7d7e6078abfc8a5a7
Negin <nuggetnegin@gmail.com>
2019-12-01 15:47:43 -0500

    fixed footer spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ class Footer extends Component {
       <footer className='footer'>
         <p>
           <span role='img' aria-label='fish emoji'>
-            🐟
+             🐟
           </span>
-          Made with big wishes by Negin Sauermann ©️ 2019{' '}
+          {' '}Made with big wishes by Negin Sauermann ©️ 2019{' '}
           <span role='img' aria-label='fish emoji' className='ripple'>
             🐟
           </span>
