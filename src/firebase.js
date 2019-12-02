bc1ed334bdbbacf9d29ad4f0a05649ed2e6c15a7
Negin <nuggetnegin@gmail.com>
2019-12-02 10:24:21 -0500

    fixed firebase error and updated title from react app to Wishy Washy

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
 // Initialize Firebase
 const firebaseConfig = {
   apiKey: 'AIzaSyBf6WRp2Vfq9FTfvids02HOQHtYQWY6HAc',
